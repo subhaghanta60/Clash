1fffc1c7ef4e7b61f3ac0f5b4a103e8fb17d2518
Subha Ghanta <Subhaghanta60@gmail.com>
2024-08-21 00:03:08 +0530

    Set up Queue in Docker and Develop Email Queue

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,16 +20,33 @@ app.set("views",path.resolve(_dirname,'./views'))
 
 
 app.get("/", async (req:Request,res:Response)=> {
+    
     const html = await ejs.renderFile(_dirname + `/views/emails/Welcome.ejs`, {name:"Subha Ghanta"})
    try {
+    /*
+    ****************Normal Sent Email**********
     await sendEmail("subhaghanta325@gmail.com","Testing SMTP", html) 
     return res.json({msg: "Email Send Successfully!"})
-    
+
+    ******************Mail Sent Using Queue**************
+    */
+    await emailQueue.add(emailQueueName,{to:"subhaghanta60@gmail.com",subject:"Testing Queue Mail",body:html})
+    return res.json({msg: "Email Send Successfully!"})
+
+
    } catch (error) {
     console.error(error);
     
    }
+    
+   await emailQueue.add(emailQueueName,{name:"Subha",age:"24"})
+   return res.json({msg: "Email Send Successfully!"})
   
 })
+
+//Queues
+
+import "./jobs/index.js"
+import { emailQueue, emailQueueName } from "./jobs/Email.Job.js"
 
 app.listen(PORT, ()=> console.log(`Server is Running on PORT ${PORT}`));
